3babd6bb30ec2a70c433707fff80642555efd8c2
Philip <philipmjobe@gmail.com>
2022-09-23 13:03:28 -0400

    adds migration to get rid of name from user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_23_165939) do
+ActiveRecord::Schema.define(version: 2022_09_23_170215) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
     t.string "first_name"
     t.string "last_name"
     t.string "email"
